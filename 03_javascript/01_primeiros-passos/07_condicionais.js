353ea45ba3627cbbe4fb6d44e1d8a38cbad3fc11
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-10-28 18:42:59 -0300

    Javascript: aula 2
    Condicionais

---
+++ 03_javascript/01_primeiros-passos/07_condicionais.js
@@ -0,0 +1,9 @@
+var valor = 30
+
+if(valor === 30){
+    console.log("Valor igual a 30!")
+} else {
+    console.log("Número diferente de 30!")
+}
+
+numero === 10 ? console.log("Valor igual a 30!") : console.log("Número diferente de 30!")
