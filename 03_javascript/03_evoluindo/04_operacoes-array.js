d2d380be5dd14c30239e7aca73a9b932aaf1cf93
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-11-04 22:49:34 -0300

    Javascript: aula 5
    Operadores MAP, REDUCE, FIND e FILTER.

---
+++ 03_javascript/03_evoluindo/04_operacoes-array.js
@@ -0,0 +1,49 @@
+// MAP
+// let lista = ["João", "Felipe", "Glória"]
+
+// lista.map((item, index) => {
+//     console.log(`Passando: ${item} - ${index}`)
+// })
+
+// =========================================================================================
+
+//REDUCE
+// let numeros = [5, 3, 2]
+
+// let total = numeros.reduce((acumulador, numero, indice, original) => {
+//     console.log(`${acumulador} - total até o momento!`)
+//     console.log(`${numero} - valor atual`)
+//     console.log(`${indice} - indice atual`)
+//     console.log(`${original} - valor original`)
+//     console.log(`==========================================`)
+
+//     return acumulador += numero
+// })
+
+// console.log("Total do reduce " + total)
+
+// =========================================================================================
+
+// FIND
+// let listagem = [5, 1, "Pedro", 5, 4, ""]
+
+// let busca = listagem.find((item) => {
+    
+//     // item === "Pedro" ?  console.log("Item encontrado") : console.log("Não está na lista") 
+//     if(item === "Pedro"){
+//         return console.log("Item encontrado")
+//     }
+
+// })
+
+// console.log(busca)
+
+// =========================================================================================
+
+let palavras = ["Kiko", "Carla", "Tamires", "Kleber", "Ana", "Bia", "Maradona"]
+
+let resultado = palavras.filter((item) => {
+    return item.length < 4
+})
+
+console.log(resultado)
