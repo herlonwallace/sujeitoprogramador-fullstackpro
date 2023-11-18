905b021524f8ae23afbf2f47d35296e9cee1c4ff
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-11-17 21:14:00 -0300

    Projeto 05 ReactJS(PrimeFlix) - Listando filmes, Loading Home e Erro página não encontrado

--- 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Home/index.js
+++ 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import api from "../../services/api"
+import { Link } from "react-router-dom"
+import "./home.css"
 
 // URL da API: /movie/550?api_key=6ab5b5c7f65a6334d298b04c923a0ea8&language=pt-BR
 
 function Home() {
     const [filmes, setFilmes] = useState([])
+    const [loanding, setLoading] = useState(true)
 
     useEffect(()=>{
         async function loadFilmes(){
@@ -16,14 +19,35 @@ function Home() {
                 }
             })
 
-            console.log(response)
+            //console.log(response)
+            setFilmes(response.data.results.slice(0, 10))
+            setLoading(false)
         }
 
         loadFilmes()
-    })
+    }, [])
+
+    if(loanding) {
+        return(
+            <div className="loanding">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Bem vindo a Home</h1>    
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>    
         </div>
     )
 }
