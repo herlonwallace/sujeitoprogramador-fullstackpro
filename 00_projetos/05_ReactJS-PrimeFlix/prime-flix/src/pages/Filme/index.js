bcc3f800e11ae19ef57adcadb6216b32e562f356
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-11-18 21:26:50 -0300

    Projeto 05 ReactJS(PrimeFlix) - Ação ver trailer do filme

--- 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Filme/index.js
+++ 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Filme/index.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate } from "react-router-dom"
 import "./filme-info.css"
 
 import api from "../../services/api"
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate()
+
     const [filme, setFilme] = useState({})
     const [loading, setLoading] = useState(true)
 
@@ -22,7 +24,8 @@ function Filme() {
                     setLoading(false)
                 })
                 .catch(() => {
-                    console.log("Filme não encotrado")
+                    navigate("/", { replace: true })
+                    return
                 })
         }
 
@@ -31,7 +34,7 @@ function Filme() {
         return () => {
             console.log("Componente foi desmontado")
         }
-    }, [])
+    }, [navigate, id])
 
     if (loading) {
         return (
@@ -52,7 +55,7 @@ function Filme() {
             <div className="area-buttons">
                 <button>Salvar</button>
                 <button>
-                    <a href="#">
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                     Trailer
                     </a>
                 </button>
