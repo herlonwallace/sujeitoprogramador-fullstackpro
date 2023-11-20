4fca1fefc94ae6a483c2c8e2b81393a974b33a9c
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-11-20 11:48:37 -0300

    Projeto 05 ReactJS(PrimeFlix) - Excluindo Filme

--- 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Favoritos/index.js
+++ 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Favoritos/index.js
@@ -10,9 +10,21 @@ function Favoritos(){
         setFilmes(JSON.parse(minhaLista) || [])
     }, [])
 
+    function exluirFilme(id){
+        let filtroFilmes = filmes.filter( (item) => {
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes)
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes))
+
+    }
+
     return(
         <div className='meus-filmes'>
             <h1>Meus filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
 
             <ul>
                 {filmes.map((item) => {
@@ -21,7 +33,7 @@ function Favoritos(){
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Exluir</button>
+                                <button onClick={() => exluirFilme(item.id)}>Exluir</button>
                             </div>
                         </li>
                     )
