019ae7db9b7ef183903ee7d658a9489405007776
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-11-20 11:32:26 -0300

    Projeto 05 ReactJS(PrimeFlix) - Criando página favoritos

--- 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/routes.js
+++ 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/routes.js
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Favoritos from "./pages/Favoritos";
 
 import Erro from "./pages/Erro";
 
 import Header from "./components/Header";
+
 
 function RoutesApp(){
     return(
@@ -14,6 +16,7 @@ function RoutesApp(){
             <Routes>
                 <Route path="/" element={ <Home/> } />
                 <Route path="/filme/:id" element={ <Filme/> } />
+                <Route path="/favoritos" element={ <Favoritos/> } />
 
                 <Route path="*" element={ <Erro/> } />
             </Routes>
