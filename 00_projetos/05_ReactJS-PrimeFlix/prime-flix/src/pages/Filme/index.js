4b6af0083b18e16824c73004d63e3e398c18485b
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2023-11-20 12:15:06 -0300

    Projeto 05 ReactJS(PrimeFlix) - Personalizando alerta

--- 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Filme/index.js
+++ 00_projetos/05_ReactJS-PrimeFlix/prime-flix/src/pages/Filme/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import "./filme-info.css"
-
+import { toast } from "react-toastify"
 import api from "../../services/api"
 
 function Filme() {
@@ -44,13 +44,13 @@ function Filme() {
         const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
 
         if(hasFilme) {
-            alert("Esse filme já esta na lista")
+            toast.warn("Esse filme já esta na lista")
             return
         }
 
         filmesSalvos.push(filme)
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
-        alert("Filme salvo com sucesso")
+        toast.success("Filme salvo com sucesso!")
     }
 
     if (loading) {
