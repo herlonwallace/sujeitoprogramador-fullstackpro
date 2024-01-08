b12f90f8cee567c74f85b1a1effc9ea0af106919
Wallace Herlon Silva <wallaceherlon@hotmail.com>
2024-01-08 19:59:58 -0300

    Projeto 06 ReactJS + TypeScript(Calculadora gasolina ou etanol)
    Concluído Front-end.

---
+++ 00_projetos/06_ReactJs-Typescript-Gasolina-Etanol/calculadora/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+import logoImg from './assets/logo.png'
+
+function App() {
+
+  return (
+    <div>
+      <main className='container'>
+        <img src={logoImg} alt="Logo da calculadora de gasolina ou álcool" />
+        <h1 className='title'>Qual melhor opção?</h1>
+
+        <form className='form'>
+          <label>Álcool (preço por litro):</label>
+          <input className='input' type="number" placeholder='4,90' min='1' step='0.01' required />
+
+          <label>Gasolina (preço por litro):</label>
+          <input className='input' type="number" placeholder='4,90' min='1' step='0.01' required />
+
+          <input className='button' type="submit" value='Calcular'/>
+        </form>
+
+      </main>
+    </div>
+  )
+}
+
+export default App
